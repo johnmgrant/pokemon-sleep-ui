d87e9b08584b0c03dfce23919be9a01b02154d74
John Mitchell-Grant <john.grant@accuweather.com>
2023-12-18 16:29:46 -0500

    Attempt to resolve dependency tree

--- src/ui/team/analysis/setup/team/main.tsx
+++ src/ui/team/analysis/setup/team/main.tsx
@@ -25,16 +25,87 @@ type Props = TeamAnalysisDataProps & TeamAnalysisFilledProps & {
   statsOfTeam: TeamProducingStats,
 };
 
-export const TeamAnalysisTeamView = (props: Props) => {
+type GrideItemProps = Props & {
+  slotName: TeamAnalysisSlotName,
+  setMember: (
+    slotName: TeamAnalysisSlotName,
+    member: TeamAnalysisMember,
+  ) => void,
+};
+
+const GridItem = (props: GrideItemProps) => {
   const {
     setup,
     setSetup,
+    slotName,
+    setMember,
     pokedexMap,
-    pokemonProducingParamsMap,
     statsOfTeam,
   } = props;
 
-  const {members, snorlaxFavorite} = getCurrentTeam({setup});
+  const {members, snorlaxFavorite} = React.useMemo(() => {
+    return getCurrentTeam({setup});
+  }, [setup]);
+
+  const member = members[slotName];
+  const pokemon = member ? pokedexMap[member.pokemonId] : undefined;
+  const stats = statsOfTeam.bySlot[slotName];
+
+  const isAvailable = member && pokemon && stats;
+  const teamAnalysisSlot = React.useMemo(() => {
+    return isAvailable ?
+      <TeamAnalysisFilledSlot
+        {...props}
+        snorlaxFavorite={snorlaxFavorite}
+        slotName={props.slotName}
+        member={member}
+        stats={stats}
+        pokemon={pokemon}
+        pokemonProducingParams={getPokemonProducingParams({
+          pokemonId: pokemon.id,
+          pokemonProducingParamsMap: props.pokemonProducingParamsMap,
+        })}
+      /> :
+      <TeamAnalysisEmptySlot
+        {...props}
+        onPokeboxPicked={(pokeInBox) => setMember(props.slotName, toTeamAnalysisMember(pokeInBox))}
+        onCloudPulled={(member) => setMember(props.slotName, member)}
+      />;
+  }, [isAvailable, member, pokemon, props, setMember, snorlaxFavorite, stats]);
+
+  return (
+    <Flex key={slotName} center className={clsx(
+      'button-bg relative gap-1.5 rounded-lg p-3',
+    )}>
+      <button
+        className={clsx(
+          'absolute right-1 top-1 z-10 h-5 w-5 rounded-full',
+          'enabled:button-clickable disabled:button-disabled-border',
+        )}
+        disabled={!member}
+        onClick={() => setSetup((original) => ({
+          ...original,
+          comps: {
+            ...original.comps,
+            [original.config.current]: getCurrentTeam({
+              setup: original,
+              overrideSlot: slotName,
+              overrideMember: null,
+            }),
+          },
+        }))}
+      >
+        <XMarkIcon/>
+      </button>
+      {teamAnalysisSlot}
+    </Flex>
+  );
+};
+
+export const TeamAnalysisTeamView = (props: Props) => {
+  const {
+    setSetup,
+  } = props;
 
   const setMember = React.useCallback((
     slotName: TeamAnalysisSlotName,
@@ -49,62 +120,16 @@ export const TeamAnalysisTeamView = (props: Props) => {
         overrideMember: member,
       }),
     },
-  })), [setSetup, getCurrentTeam]);
+  })), [setSetup]);
 
   return (
     <Grid className="grid-cols-1 gap-1.5 lg:grid-cols-3 xl:grid-cols-5">
-      {teamAnalysisSlotName.map((slotName) => {
-        const member = members[slotName];
-        const pokemon = member ? pokedexMap[member.pokemonId] : undefined;
-        const stats = statsOfTeam.bySlot[slotName];
-
-        const isAvailable = member && pokemon && stats;
-
-        return (
-          <Flex key={slotName} center className={clsx(
-            'button-bg relative gap-1.5 rounded-lg p-3',
-          )}>
-            <button
-              className={clsx(
-                'absolute right-1 top-1 z-10 h-5 w-5 rounded-full',
-                'enabled:button-clickable disabled:button-disabled-border',
-              )}
-              disabled={!member}
-              onClick={() => setSetup((original) => ({
-                ...original,
-                comps: {
-                  ...original.comps,
-                  [original.config.current]: getCurrentTeam({
-                    setup: original,
-                    overrideSlot: slotName,
-                    overrideMember: null,
-                  }),
-                },
-              }))}
-            >
-              <XMarkIcon/>
-            </button>
-            {isAvailable ?
-              <TeamAnalysisFilledSlot
-                {...props}
-                snorlaxFavorite={snorlaxFavorite}
-                slotName={slotName}
-                member={member}
-                stats={stats}
-                pokemon={pokemon}
-                pokemonProducingParams={getPokemonProducingParams({
-                  pokemonId: pokemon.id,
-                  pokemonProducingParamsMap,
-                })}
-              /> :
-              <TeamAnalysisEmptySlot
-                {...props}
-                onPokeboxPicked={(pokeInBox) => setMember(slotName, toTeamAnalysisMember(pokeInBox))}
-                onCloudPulled={(member) => setMember(slotName, member)}
-              />}
-          </Flex>
-        );
-      })}
+      {teamAnalysisSlotName.map((slotName) => <GridItem
+        {...props}
+        key={slotName}
+        slotName={slotName}
+        setMember={setMember}
+      />)}
     </Grid>
   );
 };
